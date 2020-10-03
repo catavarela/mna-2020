292c1a91ed0047f028566e5c30e5193756f10dad
gbarbieripederiva <gbarbieripederiva@gmail.com>
2020-10-03 19:23:06 -0300

    added get_face and a dependency

---
+++ facedetection.py
@@ -0,0 +1,9 @@
+# https://pypi.org/project/face-recognition/
+import face_recognition as fr
+import cv2 as cv
+
+def get_face(img):
+    face_loc = fr.face_locations(img)
+    print(face_loc)
+    loc = face_loc[0]
+    img = img[face_loc[0][0]:face_loc[0][2],face_loc[0][3]:face_loc[0][1]]
