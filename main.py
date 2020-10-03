a90579915fc36cf0db541ad4f723d97c48b20e5a
Agustín Roca <aroca@itba.edu.ar>
2020-10-03 17:51:44 -0300

    Change in generate_eigenfaces
    Change a calculation in order to match exactly on how the sample paper was doing it

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from itertools import chain
 import random
 import time
 
-images_to_use = 500
+images_to_use = 100
 # Source: http://ijarcet.org/wp-content/uploads/IJARCET-VOL-1-ISSUE-9-135-139.pdf
+
+
 def generate_eigenfaces(root_dir, keep_percentage=0.5):
-    images = glob.glob(root_dir + '/**/*.jpg', recursive=True)
+    images = glob.glob(root_dir + '/**/*0[1-3].jpg', recursive=True)
+    images.sort()
     faces = []
     for i in range(0, images_to_use):  # TODO: cambiar el images_to_use por len(faces)
         # Step 1
@@ -26,49 +29,72 @@ def generate_eigenfaces(root_dir, keep_percentage=0.5):
     # Step 5
     # Al final no se usa la covarianza asi que no la calculo
     # Step 6
-    l = faces_not_average.transpose() @ faces_not_average
-    eigval, l_eigvec = np.linalg.eig(l)  # TODO: usar nuestra funcion de autovals y autovecs
+    # TODO: usar nuestra funcion de autovals y autovecs
+    eigval, l_eigvec = np.linalg.eig(faces_not_average.transpose() @ faces_not_average)
     # Quiero los eigvec ordenados por mayor eigval
-    ordered_l_eigvec = [vec for val, vec in sorted(zip(eigval,l_eigvec), reverse=True)]
-    ordered_eigvec = faces_not_average @ ordered_l_eigvec
+    ordered_l_eigvec = [vec for val, vec in sorted(zip(eigval, l_eigvec), reverse=True)]
+    ordered_eigvec = []
+    v = np.array(ordered_l_eigvec)  # solo por paralelismo con el paper
+    for l in range(len(v)):
+        u_l = np.zeros((len(faces_not_average)))
+        for k in range(len(v[l])):
+            u_l += v[l][k] * faces_not_average[:, k]
+        ordered_eigvec.append(np.array(u_l).transpose())
     # Step 7
-    sliced_eigvec = ordered_eigvec.transpose()
-    return sliced_eigvec[0:int(len(sliced_eigvec) * keep_percentage), :], avg
+    ordered_eigvec = np.array(ordered_eigvec)
+    return ordered_eigvec[0:int(len(ordered_eigvec) * keep_percentage), :], avg
 
 
 def get_weights(face, eigenfaces, avg):
-    face = np.array(list(chain.from_iterable(face)))
+    face = np.array(face).reshape(1, -1)[0]
     weights = []
     for i in range(0, len(face)):
         face[i] -= avg[i]
     for i in range(0, len(eigenfaces)):
         weights.append(float(eigenfaces[i] @ face.reshape(-1, 1)))
-    return weights
+    return np.array(weights)
 
 
 start = time.time()
 eigenfaces, avg = generate_eigenfaces('data')
 print('Eigenfaces generated')
+print('Generated in:', time.time() - start, 's')
+# for eigenface in eigenfaces:
+#     eig_img = np.zeros((250, 250), dtype=np.uint8)
+#     for i in range(0, len(avg)):
+#         eig_img[i // 250][i % 250] = eigenface[i]
+#     cv.imshow('average', eig_img)
+#     cv.waitKey(0)
 weights = []
-images = glob.glob('data/**/*.jpg', recursive=True)
+images1 = glob.glob('data/**/*0[1-3].jpg', recursive=True)
+images1.sort()
 for i in range(0, images_to_use):  # TODO: cambiar el images_to_use por len(images)
-    face = cv.imread(images[i])
+    face = cv.imread(images1[i])
     face = cv.cvtColor(face, cv.COLOR_RGB2GRAY)
     weights.append(get_weights(face, eigenfaces, avg))
 print('Weights calculated')
+images2 = glob.glob('data/**/*4.jpg', recursive=True)
+images2.sort()
 random.seed()
-rand = int(random.random() * len(eigenfaces))
-print('Imagen de prueba:', images[rand])
-test_face = cv.imread(images[rand])
+rand = int(random.random() * (images_to_use//3))
+print('Imagen de prueba:', images2[rand])
+test_face = cv.imread(images2[rand])
 test_face = cv.cvtColor(test_face, cv.COLOR_RGB2GRAY)
 test_weight = np.array(get_weights(test_face, eigenfaces, avg))
-min_distance = np.linalg.norm(test_weight - weights[0])
+min_distance = -1
 min_i = 0
-for i, w in enumerate(weights):
-    aux = np.linalg.norm(test_weight - w)
-    if aux < min_distance:
-        min_distance = aux
-        min_i = i
-print('La imagen es:', images[min_i])
+accum = 0
+for index, weight in enumerate(weights):
+    difference = []
+    for i in range(0, len(weight)):
+        difference.append(weight[i] - test_weight[i])
+    accum += np.linalg.norm(difference)
+    if (index + 1) % 3 == 0:
+        distance = accum / 3
+        accum = 0
+        if distance < min_distance or min_distance == -1:
+            min_distance = distance
+            min_i = index
 end = time.time()
+print('La imagen es:', images1[min_i])
 print('Tardé', end - start, 's')
