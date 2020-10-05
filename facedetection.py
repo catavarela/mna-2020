7ca5745dfa4ad6353f788bfc2b0ac2735b228f21
nicbritos <nicbritos@hotmail.com>
2020-10-04 22:35:55 -0300

    Added pre processing of images to get more accurate results

--- facedetection.py
+++ facedetection.py
@@ -1,11 +1,33 @@
 # https://pypi.org/project/face-recognition/
-import face_recognition as fr
+import math
+
 import cv2 as cv
 
 
-def get_face(img, width=150, height=150):
-    face_loc = fr.face_locations(img)
-    if len(face_loc) > 0:
-        img = img[face_loc[0][0]:face_loc[0][2], face_loc[0][3]:face_loc[0][1]]
-    img = cv.resize(img, (width, height))
-    return img
+class FaceRecognition:
+    def __init__(self):
+        self.face_classifier = cv.CascadeClassifier(cv.data.haarcascades + './haarcascade_frontalface_default.xml')
+        self.eyes_classifier = cv.CascadeClassifier(cv.data.haarcascades + './haarcascade_eye.xml')
+
+    def get_face(self, img, width=150, height=150):
+        faces_detected = self.face_classifier.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5)
+
+        if len(faces_detected) > 0:
+            (x, y, w, h) = faces_detected[0]
+
+            eyes = self.eyes_classifier.detectMultiScale(img[y:y + h, x:x + w])
+
+            if len(eyes) == 2:
+                rows, cols = img.shape[:2]
+                center_eyes = [
+                    [(eyes[0][0] + eyes[0][2]) / 2, (eyes[0][1] + eyes[0][3]) / 2],
+                    [(eyes[1][0] + eyes[1][2]) / 2, (eyes[1][1] + eyes[1][3]) / 2],
+                ]
+                angle = math.atan2(center_eyes[1][1] - center_eyes[0][1], center_eyes[1][0] - center_eyes[0][0])
+                M = cv.getRotationMatrix2D((cols / 2, rows / 2), math.degrees(angle), 1)
+                img = cv.warpAffine(img, M, (cols, rows))
+
+            img = img[y + 1: y + h, x + 1: x + w]
+
+        img = cv.resize(img, (width, height), interpolation=cv.INTER_LINEAR)
+        return img
--- main.py
+++ main.py
@@ -3,31 +3,33 @@ import glob
 import numpy as np
 import random
 import time
-import facedetection as fd
+
+from Matrix_Handling_Functions import get_eigen_from_qr
+from facedetection import FaceRecognition
 
 # Source: https://www.mitpressjournals.org/doi/pdf/10.1162/jocn.1991.3.1.71
 
 
-def get_face_as_row(path, side_length=150):
+def get_face_as_row(face_recognition: FaceRecognition, path, side_length=150):
     face = cv.imread(path)
-    face = fd.get_face(face, side_length, side_length)
+    face = face_recognition.get_face(face, side_length, side_length)
     face = cv.cvtColor(face, cv.COLOR_RGB2GRAY)
     return np.reshape(face, side_length * side_length)
 
 
-def get_face_as_column(path, side_length=150):
-    return get_face_as_row(path, side_length)[:, np.newaxis]
+def get_face_as_column(face_recognition, path, side_length=150):
+    return get_face_as_row(face_recognition, path, side_length)[:, np.newaxis]
 
 
-def get_faces_as_columns(paths, side_length=150, images_to_use=100):
+def get_faces_as_columns(face_recognition, paths, side_length=150, images_to_use=100):
     faces = np.zeros((images_to_use, side_length * side_length))
     for i in range(0, images_to_use):  # TODO: cambiar el images_to_use por len(faces)
-        faces[i] = get_face_as_row(paths[i], side_length)
+        faces[i] = get_face_as_row(face_recognition, paths[i], side_length)
     return np.transpose(faces)
 
 
-def generate_eigenfaces(paths, keep_percentage=0.5):
-    faces = get_faces_as_columns(paths)
+def generate_eigenfaces(face_recognition, paths, keep_percentage=0.5):
+    faces = get_faces_as_columns(face_recognition, paths)
 
     # Calculo la media
     avg = np.mean(faces, 1)[:, np.newaxis]
@@ -37,6 +39,7 @@ def generate_eigenfaces(paths, keep_percentage=0.5):
     # Calculo los mayores autovectores de la covarianza usando el truco del paper
     L = faces_min_avg.transpose() @ faces_min_avg
     eigval, L_eigvec = np.linalg.eig(L)  # TODO: usar nuestra funcion de autovals y autovecs
+    # eigval, L_eigvec = get_eigen_from_qr(L)
     # Quiero los autovectores ordenados por mayor autovalor
     v = [vec for val, vec in sorted(zip(eigval, L_eigvec), reverse=True)]
     u = np.zeros((len(v), len(faces_min_avg)))  # Autovectores de la covarianza
@@ -57,50 +60,58 @@ def get_weights(face, eigenfaces, avg):
     return weights
 
 
-images_to_use = 100
-start = time.time()
+def main():
+    face_recognition = FaceRecognition()
 
-# ENTRENAMIENTO
-training_images = glob.glob('data/**/*0[1-3].jpg', recursive=True)
-training_images.sort()
-eigenfaces, avg = generate_eigenfaces(training_images)
-print('Eigenfaces generated in:', time.time() - start, 's')
+    images_to_use = 100
+    start = time.time()
 
-weights = []
+    # ENTRENAMIENTO
+    training_images = glob.glob('data/**/*0[1-3].jpg', recursive=True)
+    training_images.sort()
+    eigenfaces, avg = generate_eigenfaces(face_recognition, training_images)
+    print('Eigenfaces generated in:', time.time() - start, 's')
 
-partial_time = time.time()
-for i in range(0, images_to_use):  # TODO: cambiar el images_to_use por len(images)
-    face = get_face_as_column(training_images[i])
-    weights.append(get_weights(face, eigenfaces, avg))
-print('Weights calculated in:', time.time() - partial_time)
-print('Training completed in:', time.time() - start)
+    weights = []
 
-# PRUEBA
-images = glob.glob('data/**/*4.jpg', recursive=True)
-images.sort()
-random.seed()
-rand = int(random.random() * (images_to_use//3))
-test_image_path = images[rand]
-print('Imagen de prueba:', test_image_path)
-partial_time = time.time()
-test_face = get_face_as_column(test_image_path)
-test_weight = get_weights(test_face, eigenfaces, avg)
-# Calculo distancias
-min_distance = -1
-min_i = 0
-accum = 0
-for index, weight in enumerate(weights):
-    difference = []
-    for i in range(0, len(weight)):
-        difference.append(weight[i] - test_weight[i])
-    distance = np.linalg.norm(difference)
-    if distance < min_distance or min_distance == -1:
-        min_distance = distance
-        min_i = index
-end = time.time()
-print('La imagen es:', training_images[min_i])
-print('Tardé', time.time() - partial_time, 's en encontrarla')
-print('Tardé en total', end - start, 's')
-cv.imshow('test', cv.imread(test_image_path))
-cv.imshow('result', cv.imread(training_images[min_i]))
-cv.waitKey(0)
+    partial_time = time.time()
+    for i in range(0, images_to_use):  # TODO: cambiar el images_to_use por len(images)
+        face = get_face_as_column(face_recognition, training_images[i])
+        weights.append(get_weights(face, eigenfaces, avg))
+    print('Weights calculated in:', time.time() - partial_time)
+    print('Training completed in:', time.time() - start)
+
+    # PRUEBA
+    images = glob.glob('data/**/*4.jpg', recursive=True)
+    images.sort()
+    random.seed()
+    rand = int(random.random() * (images_to_use // 3))
+    test_image_path = images[rand]
+    print('Imagen de prueba:', test_image_path)
+    partial_time = time.time()
+    test_face = get_face_as_column(face_recognition, test_image_path)
+    test_weight = get_weights(test_face, eigenfaces, avg)
+    # Calculo distancias
+    min_distance = -1
+    min_i = 0
+    accum = 0
+    for index, weight in enumerate(weights):
+        difference = []
+        for i in range(0, len(weight)):
+            difference.append(weight[i] - test_weight[i])
+        distance = np.linalg.norm(difference)
+        if distance < min_distance or min_distance == -1:
+            min_distance = distance
+            min_i = index
+
+    end = time.time()
+    print('La imagen es:', training_images[min_i])
+    print('Tardé', time.time() - partial_time, 's en encontrarla')
+    print('Tardé en total', end - start, 's')
+    cv.imshow('test', cv.imread(test_image_path))
+    cv.imshow('result', cv.imread(training_images[min_i]))
+    cv.waitKey(0)
+
+
+if __name__ == '__main__':
+   main()
