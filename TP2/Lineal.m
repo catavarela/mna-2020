e01f5a155dc7d925532c697e464ccc77429bdfdc
ahenestrosa <ahenestrosa@itba.edu.ar>
2020-11-22 13:35:16 -0300

    fix in main

--- TP2/Lineal.m
+++ TP2/Lineal.m
@@ -1,7 +1,3 @@
 function U = Lineal(delta_t, U, k)
-% This function calculates the Linear part analitically
-% given a function, a delta_t and the values k.
-
 U = U.*exp((k.^2 - k.^4)*delta_t);
-
 end
--- TP2/main.m
+++ TP2/main.m
@@ -1,37 +1,31 @@
-% Analysis constants
-N = 256; %number of discrete
+N = 256;
+
 tic
-% interval defined
 IntStart = 0;
 IntFin = 32 * pi;
-x = IntervalDivider(IntStart, IntFin,N); %discretization of interval
+
+x = IntervalDivider(IntStart, IntFin,N); 
 d_t = 0.01;
 d_x = x(2) - x(1);
 d_k = (2*pi)/(N*(x(2)-x(1)));
 k = [0:N/2-1 0 -N/2+1:-1]' * d_k;
 
-% Render constants
-frames = 10;
-tensoruu = {};
-tensortt = {};
-q=4
+q=4; frames = 10;
+tensoruu = {}; tensortt = {};
 
-% Solver method
 for i = 1:frames
   [tt, uu] = Solver(d_t,x,k,q,1,@LieTrotter);
   tensoruu = [tensoruu uu];
   tensortt = [tensortt tt];
 end
 
-% Render cycle
-% while true
 h = figure;
 filename = 'Movement.gif';
 for i = 1:frames
   tt = tensortt{i};
   uu = tensoruu{i};
 
-  % Plot results:
+  % ---------------- Plotting -----------------------------
   surf(tt, x, uu), shading interp, lighting phong, axis tight
   view([-90 90]), colormap(autumn); set(gca, 'zlim', [-5 50])
   light('color', [1 1 0], 'position', [-1, 2, 2])
@@ -48,5 +42,4 @@ for i = 1:frames
 end
 % end
 toc
-
-delete(gcp('nocreate')); %to shutdown parpool
+delete(gcp('nocreate'));
